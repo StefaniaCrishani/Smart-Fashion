f4e93b375eac467303f2627efde45a5e307a9c0b
StefaniaCrishani <stefania.crishani99@gmail.com>
2020-08-15 14:26:33 +0530

    commit message goes here

---
+++ app/src/main/java/com/example/smartfashion/ProductMain.java
@@ -0,0 +1,22 @@
+package com.example.smartfashion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class ProductMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_main);
+
+        /*public void editProduct(View view){
+
+            Intent intent1=new Intent(packageContext this, ProductMain.class);
+            startActivity(intent1);
+        }*/
+    }
+}
