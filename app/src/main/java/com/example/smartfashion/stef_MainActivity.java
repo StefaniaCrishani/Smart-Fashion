09ea09e53c68b651ef47cd6c443313494d3e62ba
StefaniaCrishani <stefania.crishani99@gmail.com>
2020-10-01 01:27:35 +0530

    commit new modification

--- app/src/main/java/com/example/smartfashion/stef_MainActivity.java
+++ app/src/main/java/com/example/smartfashion/stef_MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.smartfashion;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
@@ -11,13 +12,16 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class stef_MainActivity extends AppCompatActivity {
 
     EditText txtpid,txtname,txtprice,txtcolors,txtsizes;
-    Button butpost;
+    Button butpost,butsave;
     DatabaseReference dbRef;
     Product prd;
 
@@ -33,24 +37,25 @@ public class stef_MainActivity extends AppCompatActivity {
         txtcolors = findViewById(R.id.colors);
 
         butpost=findViewById(R.id.post);
+        butsave=findViewById(R.id.save);
 
         prd = new Product();
 
-        butpost.setOnClickListener(new View.OnClickListener() {
+        butsave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 dbRef = FirebaseDatabase.getInstance().getReference().child("Product");
                 try {
                     if (TextUtils.isEmpty(txtpid.getText().toString()))
-                        Toast.makeText(getApplicationContext(), "Enter product ID", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getApplicationContext(), "Empty product ID", Toast.LENGTH_SHORT).show();
                     else if (TextUtils.isEmpty(txtname.getText().toString()))
-                        Toast.makeText(getApplicationContext(), "Enter product name", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getApplicationContext(), "Empty product name", Toast.LENGTH_SHORT).show();
                     else if (TextUtils.isEmpty(txtprice.getText().toString()))
-                        Toast.makeText(getApplicationContext(), "Enter product price", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getApplicationContext(), "Empty product price", Toast.LENGTH_SHORT).show();
                     else if (TextUtils.isEmpty(txtsizes.getText().toString()))
-                        Toast.makeText(getApplicationContext(), "Enter product sizes", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getApplicationContext(), "Empty product sizes", Toast.LENGTH_SHORT).show();
                     else if (TextUtils.isEmpty(txtcolors.getText().toString()))
-                        Toast.makeText(getApplicationContext(), "Enter product colors", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getApplicationContext(), "Empty product colors", Toast.LENGTH_SHORT).show();
                     else {
                         prd.setPid(txtpid.getText().toString().trim());
                         prd.setName(txtname.getText().toString().trim());
@@ -67,6 +72,33 @@ public class stef_MainActivity extends AppCompatActivity {
                 } catch (NumberFormatException e) {
                     Toast.makeText(getApplicationContext(), "Invalid price", Toast.LENGTH_SHORT).show();
                 }
+            }
+        });
+
+        butsave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatabaseReference readRef = FirebaseDatabase.getInstance().getReference().child("Product").child("prd1");
+                readRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                        if (dataSnapshot.hasChildren()) {
+                            txtpid.setText(dataSnapshot.child("pid").getValue().toString());
+                            txtname.setText(dataSnapshot.child("name").getValue().toString());
+                            txtprice.setText(dataSnapshot.child("price").getValue().toString());
+                            txtsizes.setText(dataSnapshot.child("sizes").getValue().toString());
+                            txtcolors.setText(dataSnapshot.child("colors").getValue().toString());
+                        } else
+                            Toast.makeText(getApplicationContext(), "No Sourse to display", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+
+                });
             }
         });
 
